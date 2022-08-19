5c4f5d4aa1aaa47c6a2807e6438efc974db208f7
Daylinn <dhart5@students.asurams.edu>
2022-08-19 12:46:36 -0400

    initial commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/WW90au8IAeEscaUI9g3AlUqEmtQwEz46",
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
